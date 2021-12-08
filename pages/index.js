f5a45303588905abdd4e8ececfc596163037816c
yougonnagetyours <michal.potoczny.92@gmail.com>
2021-12-08 09:23:00 +0100

    create global styles

--- pages/index.js
+++ pages/index.js
@@ -8,7 +8,7 @@ const posts = [
 
 export default function Home() {
   return (
-    <div className="container mx-auto px-10 mb-8 bg-gray-300">
+    <div className="container mx-auto px-10 mb-8">
       <Head>
         <title>Travelfolio</title>
         <link rel="icon" href="/favicon.ico" />
@@ -16,16 +16,12 @@ export default function Home() {
       
       <div className="grid grid-cols-1 lg:grid-cols-12 gap-12">
         <div className="lg:col-span-8 col-span-1">
-          {posts.map((post, index) => (
-            <div>
-              {post.title}
-              {post.excerpt}
-            </div>
-          ))}
+          {posts.map((post) => <PostCard post={post} />)}
         </div>
         <div className="lg:col-span-4 col-span-1">
             <div className="lg:sticky relative top-8">
-
+              <PostWidget />
+              <Categories />
             </div>
         </div>
       </div>  
