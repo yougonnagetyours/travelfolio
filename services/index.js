4c49847157932997b0208b0835be1fc3122c5fb6
yougonnagetyours <michal.potoczny.92@gmail.com>
2021-12-08 10:38:17 +0100

    first gql query

---
+++ services/index.js
@@ -0,0 +1,33 @@
+import { request, gql } from 'graphql-request'
+
+export const getPosts = async () => {
+  const query = gql`
+  query MyQuery {
+    postsConnection {
+      edges {
+        node {
+          author {
+            bio
+            name
+            id
+            photo {
+              url
+            }
+          }
+          createdAt
+          slug
+          title
+          excerpt
+          featuredImage {
+            url
+          }
+          categories {
+            name
+            slug
+          }
+        }
+      }
+    }
+  }  
+  `
+}
