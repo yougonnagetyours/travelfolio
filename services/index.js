9fb5296c31fd753b503a20e3a2d862950ad76085
yougonnagetyours <michal.potoczny.92@gmail.com>
2021-12-08 13:15:51 +0100

    post cards finish

--- services/index.js
+++ services/index.js
@@ -4,36 +4,36 @@ const graphqlAPI = process.env.NEXT_PUBLIC_GRAPHCMS_ENDPOINT;
 
 export const getPosts = async () => {
   const query = gql`
-  query MyQuery {
-    postsConnection {
-      edges {
-        node {
-          author {
-            bio
-            name
-            id
-            photo {
+    query MyQuery {
+      postsConnection {
+        edges {
+          node {
+            author {
+              bio
+              name
+              id
+              photo {
+                url
+              }
+            }
+            createdAt
+            slug
+            title
+            excerpt
+            featuredImage {
               url
             }
-          }
-          createdAt
-          slug
-          title
-          excerpt
-          featuredImage {
-            url
-          }
-          categories {
-            name
-            slug
+            categories {
+              name
+              slug
+            }
           }
         }
       }
-    }
-  }  
+    }  
   `
 
-  const result = await request(graphglAPI, query)
+  const result = await request(graphqlAPI, query)
 
   return result.postsConnection.edges;
 }
