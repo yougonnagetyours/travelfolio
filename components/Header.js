997eb9f17d17d145f665c7dc641ad49d345da608
yougonnagetyours <michal.potoczny.92@gmail.com>
2021-12-08 10:21:37 +0100

    set header with dummy categories

--- components/Header.js
+++ components/Header.js
@@ -1,9 +1,35 @@
-import React from 'react'
+import React, { useContext } from 'react'
+
+import Link from 'next/link'
+
+const categories = [
+  {name: 'React', slug: 'react'},
+  {name: 'Travels', slug: 'travels'},
+  {name: 'Equipment', slug: 'equipment'},
+  {name: 'Car', slug: 'car'},
+]
 
 const Header = () => {
   return (
-    <div>
-      
+    <div className="container mx-auto px-10 mb-8">
+      <div className="border-b w-full inline-block border-blue-400 py-8">
+        <div className="md:float-left block">
+          <Link href="/">
+            <span className="cursor-pointer font-bold text-4xl text-white">
+              Travelfolio
+            </span>
+          </Link>
+        </div>
+        <div className="hidden md:float-left md:contents">
+          {categories.map((category) => (
+            <Link key={category.slug} href={`/category/${category.slug}`}>
+              <span className="md:float-right mt-2 align-middle text-white ml-4 font-semibold cursor-pointer">
+                {category.name}
+              </span>
+            </Link>
+          ))}
+        </div>
+      </div>
     </div>
   )
 }
--- pages/_app.js
+++ pages/_app.js
@@ -1,9 +1,15 @@
-import 'tailwindcss/tailwind.css'
+import React, {useState, useEffect} from 'react'
+import { Layout  } from '../components'
 
+import 'tailwindcss/tailwind.css'
 import '../styles/globals.scss'
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+  <Layout>
+    <Component {...pageProps} />
+  </Layout>
+  )
 }
 
 export default MyApp
