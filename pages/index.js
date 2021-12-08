a192d1912db0be28d957a4edb3fe040c0f12d25f
yougonnagetyours <michal.potoczny.92@gmail.com>
2021-12-08 09:14:32 +0100

    create first components

--- pages/index.js
+++ pages/index.js
@@ -1,14 +1,34 @@
 import Head from 'next/head'
+import { PostCard, Categories, PostWidget } from '../components'
+
+const posts = [
+  { title: 'React Testing', excerpt: 'Learn React Testing'},
+  { title: 'React with Tailwind', excerpt: 'Learn React with Taiwind'},
+]
 
 export default function Home() {
   return (
-    <div className="container mx-auto px-10 mb-8">
+    <div className="container mx-auto px-10 mb-8 bg-gray-300">
       <Head>
         <title>Travelfolio</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-
       
+      <div className="grid grid-cols-1 lg:grid-cols-12 gap-12">
+        <div className="lg:col-span-8 col-span-1">
+          {posts.map((post, index) => (
+            <div>
+              {post.title}
+              {post.excerpt}
+            </div>
+          ))}
+        </div>
+        <div className="lg:col-span-4 col-span-1">
+            <div className="lg:sticky relative top-8">
+
+            </div>
+        </div>
+      </div>  
     </div>
   )
 }
