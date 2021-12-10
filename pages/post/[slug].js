d0b50aff152eb717885df8bb01b6cc38a2e83b48
Michał Potoczny <michal.potoczny.92@gmail.com>
2021-12-10 15:21:50 +0000

    PostDetails init

---
+++ pages/post/[slug].js
@@ -0,0 +1,16 @@
+import React, { useState, useEffect } from 'react'
+
+import { getPosts, getPostDetails } from '../../services'
+
+import { PostDetail, Categories, PostWidget, Author, Comments, CommentsForm } from '../../components'
+ 
+const PostDetails = () => {
+
+  return (
+    <div className=''>
+      
+    </div>
+  )
+}
+
+export default PostDetails
