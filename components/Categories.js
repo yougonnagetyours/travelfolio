cc703925807159a261cbcb0632b1c02b28bda082
Michał Potoczny <michal.potoczny.92@gmail.com>
2021-12-10 14:14:23 +0000

    come back to origin

--- components/Categories.js
+++ components/Categories.js
@@ -7,8 +7,8 @@ const Categories = () => {
   const [categories, setCategories] = useState([]);
 
   useEffect(() => {
-    getCategories().then((result) => {
-      setCategories(result);
+    getCategories().then((newCategories) => {
+      setCategories(newCategories);
     });
   }, []);
 
