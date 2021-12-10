86f499971ab608cbc7ac990ee0d4f19e15481736
Michał Potoczny <michal.potoczny.92@gmail.com>
2021-12-10 12:10:10 +0000

    come back

--- components/PostWidget.js
+++ components/PostWidget.js
@@ -4,7 +4,7 @@ import Link from 'next/link'
 
 import { getRecentPosts, getSimilarPosts } from '../services'
 
-const PostWidget = () => {
+const PostWidget = ({ categories, slug }) => {
   const [relatedPosts, setRelatedPosts] = useState([])
 
   useEffect(() => {
