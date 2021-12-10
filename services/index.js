fd22748e93bf3d1eb68a46c1c6558c6596138d5b
Michał Potoczny <michal.potoczny.92@gmail.com>
2021-12-10 16:25:14 +0000

    fix bug attempt

--- services/index.js
+++ services/index.js
@@ -93,7 +93,7 @@ export const getRecentPosts = async () => {
   return result.posts;
 }
 
-export const getSimilarPosts = async () => {
+export const getSimilarPosts = async (categories, slug) => {
   const query = gql`
     query GetPostDetails($slug: String!, $categories: [String!]) {
       posts(
