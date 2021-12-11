4479952fb079bce50948089b19782a3a2d740e1f
Michał Potoczny <michal.potoczny.92@gmail.com>
2021-12-11 19:23:10 +0100

    Update [slug].js

--- pages/post/[slug].js
+++ pages/post/[slug].js
@@ -7,7 +7,7 @@ import { PostDetail, Categories, PostWidget, Author, Comments, CommentsForm } fr
 const PostDetails = ({ post }) => {
 
   return (
-    <div className='container mx-auto px-10 mb-8'>
+    <div className='container mx-auto px-4 sm:px-10 mb-8'>
       <div className='grid grid-cols-1 lg:grid-cols-12 gap-12'>
         <div className='col-span-1 lg:col-span-8'>
             <PostDetail post={post} />
