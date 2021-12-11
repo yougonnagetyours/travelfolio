62f2f283598acd6b11717f2a2a1113cca54183a6
yougonnagetyours <michal.potoczny.92@gmail.com>
2021-12-11 11:36:21 +0100

    commit

--- pages/post/[slug].js
+++ pages/post/[slug].js
@@ -43,6 +43,6 @@ export async function getStaticPaths() {
 
     return {
         paths: posts.map(({ node: { slug }}) => ({ params: { slug }})),
-        fallback: false,
+        fallback: true,
     }
 }
