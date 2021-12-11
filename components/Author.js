89c843a3a7b0ba15a8ce5cdc0eb61a0c994a931b
yougonnagetyours <michal.potoczny.92@gmail.com>
2021-12-11 14:12:43 +0100

    set up content in ProjectDetails

--- components/Author.js
+++ components/Author.js
@@ -1,9 +1,21 @@
-import React from 'react'
+import React from 'react';
+import Image from 'next/image';
 
-const Author = () => {
+const Author = ({ author }) => {
   return (
-    <div className=''>
-      <h1>Author</h1>
+    <div className="text-center mt-20 mb-8 p-12 relative rounded-lg bg-black bg-opacity-20">
+      <div className="absolute left-0 right-0 -top-14">
+        <img
+          //unoptimized
+          alt={author.name}
+          height="100px"
+          width="100px"
+          className="align-middle cover rounded-full mx-auto"
+          src={author.photo.url}
+        />
+      </div>
+      <h3 className="text-white mt-8 mb-4 text-xl font-bold">{author.name}</h3>
+      <p className="text-white text-ls">{author.bio}</p>
     </div>
   )
 }
