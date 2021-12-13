126b69886838c27302437269c0373642b7326d9a
Michał Potoczny <michal.potoczny.92@gmail.com>
2021-12-13 12:47:01 +0000

    last approach to nav menu styling

--- components/Header.js
+++ components/Header.js
@@ -27,10 +27,10 @@ const Header = () => {
             <div className="relative">
               <div className='flex'>
                   <div className="relative mt-2 align-middle text-white ml-4 font-semibold cursor-pointer">Blog
-                    <div className="absolute z-50 -right-1/4 top-full mt-4 mb-8  p-8 pb-12 bg-white shadow-lg rounded-lg">
+                    <div className="absolute z-50 -right-1/4 top-full mt-4 mb-8  p-8 pb-12 bg-white color-black shadow-lg rounded-lg">
                       {categories.map((category) => (
                         <Link key={category.slug} href={`/category/${category.slug}`}>
-                          <span className="mt-2 align-middle ml-4 font-semibold cursor-pointer">
+                          <span className="mt-2 align-middle font-semibold cursor-pointer">
                             {category.name}
                           </span>
                         </Link>
