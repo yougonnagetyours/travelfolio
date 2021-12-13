d602ac934b2d9bf68ad5b4940a1e988e476997fb
Michał Potoczny <michal.potoczny.92@gmail.com>
2021-12-13 09:49:11 +0000

    style

--- components/Header.js
+++ components/Header.js
@@ -27,10 +27,10 @@ const Header = () => {
             <div className="relative">
               <div className='absolute top-0 right-0'>
                 <span className="md:float-right mt-2 align-middle text-white ml-4 font-semibold cursor-pointer">Blog</span>
-                <div className="absolute left-0">
+                <div className="absolute left-0 bg-white shadow-lg rounded-lg">
                   {categories.map((category) => (
                     <Link key={category.slug} href={`/category/${category.slug}`}>
-                      <span className="md:float-right mt-2 align-middle text-white ml-4 font-semibold cursor-pointer">
+                      <span className="md:float-right mt-2 align-middle ml-4 font-semibold cursor-pointer">
                         {category.name}
                       </span>
                     </Link>
