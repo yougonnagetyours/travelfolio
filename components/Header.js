372963486df6626b11e6d5408b105921321cd65c
Michał Potoczny <michal.potoczny.92@gmail.com>
2021-12-13 14:25:04 +0000

    styling

--- components/Header.js
+++ components/Header.js
@@ -39,7 +39,7 @@ const Header = () => {
                   <span className="mt-2 align-middle text-white ml-4 font-semibold cursor-pointer">About Me</span>  
               </div>    
         </div>
-        <div className="sm:hidden h-8">
+        <div className="sm:hidden bg-pink-100 h-8">
           <div className="border-b-2 border-white w-8 h-1/4"></div>
           <div className="border-b-2 border-white w-8 h-1/2"></div>
           <div className="w-10 h-1/4"></div>
