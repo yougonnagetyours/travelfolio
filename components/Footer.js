4c3c46aa62a7c1ffb64e886c169a05d41facfc4d
Michał Potoczny <michal.potoczny.92@gmail.com>
2021-12-14 08:17:39 +0000

    footer styling

--- components/Footer.js
+++ components/Footer.js
@@ -3,9 +3,9 @@ import { AiOutlineInstagram } from "react-icons/ai";
 
 const Footer = () => {
   return (
-    <div className="container mx-auto px-4 sm:px-10 mb-4 sm:mb-8">
-      <div className="flex justify-between border-b w-full inline-block border-blue-400 pt-4 pb-2 sm:py-8">
-        <AiOutlineInstagram />
+    <div className="container mx-auto px-4 sm:px-10 mt-4 sm:mt-8">
+      <div className="flex justify-between border-t w-full inline-block border-gray-900 pt-2 pb-4 sm:py-8">
+        <AiOutlineInstagram className='text-white' />
       </div>
     </div>
   )
