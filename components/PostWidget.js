5d9ca48499297174a5049c3e636dfb9a5365f3d8
Michał Potoczny <michal.potoczny.92@gmail.com>
2021-12-14 09:47:35 +0000

    once mooooreee

--- components/PostWidget.js
+++ components/PostWidget.js
@@ -26,7 +26,7 @@ const PostWidget = ({ categories, slug }) => {
       </h3>
       {relatedPosts.map((post) => (
         <div key={post.title} className='flex items-center w-full mb-4'>
-          <div className="w-16 flex-none">
+          <div className="h-16 flex-none">
             <img 
               src={post.featuredImage.url} 
               alt={post.title}
