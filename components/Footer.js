4800dfc17d6543922fa62fbe534840405f27d70d
Michał Potoczny <michal.potoczny.92@gmail.com>
2021-12-14 09:18:20 +0000

    footer styling

--- components/Footer.js
+++ components/Footer.js
@@ -6,8 +6,8 @@ import moment from 'moment'
 const Footer = () => {
   return (
     <div className="container mx-auto px-4 sm:px-10 mt-4 sm:mt-8">
-      <div>Made by Yougonnagetyours</div>  
-      <div className="flex justify-center border-t w-full inline-block border-gray-900 pt-2 pb-4 sm:py-8">
+      <div className='text-center'>Made by Yougonnagetyours</div>  
+      <div className="flex justify-center border-t w-full border-gray-900 pt-2 pb-4 sm:py-8">
         <div>
             <a href="">
                 <AiOutlineInstagram  size='2rem' />
