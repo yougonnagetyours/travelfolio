6d99c1e2a131b56f441592ebe79597cf3005d395
Michał Potoczny <michal.potoczny.92@gmail.com>
2021-12-14 09:15:00 +0000

    style socials

--- components/Footer.js
+++ components/Footer.js
@@ -1,17 +1,23 @@
 import React from 'react';
-import { AiOutlineInstagram } from "react-icons/ai";
+import { AiOutlineInstagram, AiFillGithub } from "react-icons/ai";
 
 import moment from 'moment'
 
 const Footer = () => {
   return (
     <div className="container mx-auto px-4 sm:px-10 mt-4 sm:mt-8">
+      <div>Made by Yougonnagetyours</div>  
       <div className="flex justify-center border-t w-full inline-block border-gray-900 pt-2 pb-4 sm:py-8">
         <div>
             <a href="">
-                <AiOutlineInstagram className='text-white' size='3rem' />
+                <AiOutlineInstagram  size='2rem' />
             </a>  
-        </div>      
+        </div>
+        <div>
+            <a href="">
+                <AiFillGithub  size='2rem' />
+            </a>  
+        </div>        
       </div>
     </div>
   )
