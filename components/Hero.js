b55d87b8c302658c1fd6ac0227bb0bbaa9095e1e
Michał Potoczny <michal.potoczny.92@gmail.com>
2021-12-14 11:39:51 +0000

    fix bugs

--- components/Hero.js
+++ components/Hero.js
@@ -1,16 +1,14 @@
 import React from 'react'
-import moment from 'moment'
 import Link from 'next/link'
 
-const Hero = ({ post }) => {
-  console.log(post);
+const Hero = () => {
   return (
     <div className="p-0 lg:p-8 pb-12 mb-8">
         <h1 className="transition duration-700 text-center mb-8 cursor-pointer hover:text-pink-600 text-3xl font-semibold">
             Welcome to My Blog
         </h1>
         <div className="text-center">
-            <Link href={`/post/${post.slug}`}>
+            <Link href=''>
                 <span className="transition duration-500 transform hover:-translate-y-1 inline-block bg-pink-600 text-lg font-medium rounded-full text-white px-8 py-3 cursor-pointer">See the blog</span> 
             </Link>
      </div>     
