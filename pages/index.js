11cc51085b0484e14f47a8622c847d43b073b374
Michał Potoczny <michal.potoczny.92@gmail.com>
2021-12-14 10:59:20 +0000

    Add it to main page

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 import Head from 'next/head'
-import { PostCard, Categories, PostWidget } from '../components'
+import { Hero, PostCard, Categories, PostWidget } from '../components'
 import { getPosts } from '../services'
 
 export default function Home({ posts }) {
@@ -10,6 +10,7 @@ export default function Home({ posts }) {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       
+      <Hero />
       <div className="grid grid-cols-1 lg:grid-cols-12 gap-12">
         <div className="lg:col-span-8 col-span-1">
           {posts.map((post, index) => <PostCard key={index} post={post.node} />)}
