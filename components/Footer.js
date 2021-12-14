9dd146383162fb743315c9cb88a830d3facd1192
Michał Potoczny <michal.potoczny.92@gmail.com>
2021-12-14 09:29:20 +0000

    footer

--- components/Footer.js
+++ components/Footer.js
@@ -6,20 +6,20 @@ import moment from 'moment'
 const Footer = () => {
   return (
     <div className="container mx-auto px-4 sm:px-10 mt-4 sm:mt-8">
-      <div className='text-center'>{moment().format('YYYY')}</div>  
-      <div className='text-center'>Made by @yougonnagetyours</div>  
-      <div className="flex justify-center border-t w-full border-gray-900 pt-2 pb-4 sm:py-8">
-        <div>
-            <a href="">
-                <AiOutlineInstagram  size='2rem' />
-            </a>  
+        <div className='text-center'>{`@${moment().format('YYYY')}`}</div>  
+        <div className='text-center'>Made by @yougonnagetyours</div>   
+        <div className="flex justify-center border-t w-full border-gray-900 pt-2 pb-4 sm:py-8">
+            <div>
+                <a href="">
+                    <AiOutlineInstagram  size='2rem' />
+                </a>  
+            </div>
+            <div>
+                <a href="">
+                    <AiOutlineGithub  size='2rem' />
+                </a>  
+            </div>        
         </div>
-        <div>
-            <a href="">
-                <AiOutlineGithub  size='2rem' />
-            </a>  
-        </div>        
-      </div>
     </div>
   )
 }
