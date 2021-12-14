ae7d6b68c4dc6ac41f9917b42a856e6e2d8ed7a3
Michał Potoczny <michal.potoczny.92@gmail.com>
2021-12-14 13:30:03 +0000

    styling

--- pages/index.js
+++ pages/index.js
@@ -12,7 +12,7 @@ export default function Home({ posts }) {
       
       <Hero />
       <div className="grid grid-cols-1 lg:grid-cols-12 gap-12">
-        <div id="blog" className="lg:col-span-8 col-span-1">
+        <div id="blog" className="lg:col-span-8 col-span-1 mt-2">
           {posts.map((post, index) => <PostCard key={index} post={post.node} />)}
         </div>
         <div className="lg:col-span-4 col-span-1">
