4823efcbc336e115fc25408d3ec341f814d458be
Michał Potoczny <michal.potoczny.92@gmail.com>
2021-12-14 13:26:01 +0000

    another attempt

--- pages/index.js
+++ pages/index.js
@@ -4,14 +4,14 @@ import { getPosts } from '../services'
 
 export default function Home({ posts }) {
   return (
-    <div className="container mx-auto px-4 sm:px-10 mb-8">
+    <div className="container scroll-smooth mx-auto px-4 sm:px-10 mb-8">
       <Head>
         <title>Travelfolio</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
       
       <Hero />
-      <div className="scroll-smooth grid grid-cols-1 lg:grid-cols-12 gap-12">
+      <div className="grid grid-cols-1 lg:grid-cols-12 gap-12">
         <div id="blog" className="lg:col-span-8 col-span-1">
           {posts.map((post, index) => <PostCard key={index} post={post.node} />)}
         </div>
