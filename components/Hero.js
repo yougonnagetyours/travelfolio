263e01faeaac0b04e2279f18a11c66e5842af5f0
Michał Potoczny <michal.potoczny.92@gmail.com>
2021-12-14 12:21:00 +0000

    Hero styling

--- components/Hero.js
+++ components/Hero.js
@@ -3,13 +3,13 @@ import Link from 'next/link'
 
 const Hero = () => {
   return (
-    <div className="p-0 lg:p-8 my-12">
-        <h1 className="transition duration-700 text-center mb-8 cursor-pointer hover:text-pink-600 text-3xl font-semibold">
+    <div className="p-0 lg:p-8 my-40">
+        <h1 className="transition duration-700 text-justify mb-8 cursor-pointer hover:text-pink-600 text-3xl font-semibold">
             Welcome to My Blog
         </h1>
         <div className="text-center">
             <Link href=''>
-                <span className="transition duration-500 transform hover:-translate-y-1 inline-block bg-pink-600 text-lg font-medium rounded-full text-white px-8 py-3 cursor-pointer">See the blog</span> 
+                <span className="w-full sm:w-auto transition duration-500 transform hover:-translate-y-1 inline-block bg-pink-600 text-lg font-medium rounded-full text-white px-8 py-3 cursor-pointer">See the blog</span> 
             </Link>
      </div>     
     </div>
