2e18509f62d9f4e9f22f9d7d3757736fcc595b13
Michał Potoczny <michal.potoczny.92@gmail.com>
2021-12-14 09:23:45 +0000

    add year

--- components/Footer.js
+++ components/Footer.js
@@ -6,7 +6,8 @@ import moment from 'moment'
 const Footer = () => {
   return (
     <div className="container mx-auto px-4 sm:px-10 mt-4 sm:mt-8">
-      <div className='text-center'>Made by Yougonnagetyours</div>  
+      <div className='text-center'>{moment().format('YYYY')}</div>  
+      <div className='text-center'>Made by @yougonnagetyours</div>  
       <div className="flex justify-center border-t w-full border-gray-900 pt-2 pb-4 sm:py-8">
         <div>
             <a href="">
