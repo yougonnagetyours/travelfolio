19991ce2ef9c4d344f0d0fd725e846cd65b0375d
Michał Potoczny <michal.potoczny.92@gmail.com>
2021-12-14 12:38:25 +0000

    hero description

--- components/Hero.js
+++ components/Hero.js
@@ -4,12 +4,15 @@ import Link from 'next/link'
 const Hero = () => {
   return (
     <div className="p-0 lg:p-8 my-40">
-        <h1 className="transition duration-700 text-center sm:text-left mb-8 cursor-pointer hover:text-pink-600 text-3xl font-semibold">
+        <h1 className="text-center sm:text-left mb-8 text-3xl font-semibold">
             Welcome to My Blog
         </h1>
+        <div className="text-center sm:text-left mb-8 text-sm font-semibold">
+            I love traveling and I want to share with you my little bit experience in it.
+        </div>
         <div className="text-center sm:text-left">
             <Link href=''>
-                <span className="w-full sm:w-auto transition duration-500 transform hover:-translate-y-1 inline-block bg-pink-600 text-lg font-medium rounded-lg sm:rounded-full text-white px-8 py-1 sm:py-3 cursor-pointer">See the blog</span> 
+                <span className="w-full sm:w-auto opacity-80 transition duration-500 transform hover:-translate-y-1 inline-block bg-pink-600 text-lg font-medium rounded-lg sm:rounded-full text-white px-8 py-1 sm:py-3 cursor-pointer">See the blog</span> 
             </Link>
      </div>     
     </div>
