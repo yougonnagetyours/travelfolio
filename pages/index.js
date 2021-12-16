5e1a64716eced97da44cf19c075b1f0ab959db09
yougonnagetyours <michal.potoczny.92@gmail.com>
2021-12-16 05:40:19 +0100

    styling (check postCard padding bottom)

--- pages/index.js
+++ pages/index.js
@@ -4,7 +4,7 @@ import { getPosts } from '../services'
 
 export default function Home({ posts }) {
   return (
-    <div className="container scroll-smooth mx-auto px-4 sm:px-10 mb-8">
+    <div className="container mx-auto px-4 sm:px-10 mb-8">
       <Head>
         <title>Travelfolio</title>
         <link rel="icon" href="/favicon.ico" />
