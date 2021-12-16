8f7781cbb612dceaec7a5a037a84720f78c0a516
Michał Potoczny <michal.potoczny.92@gmail.com>
2021-12-16 07:23:08 +0000

    style

--- components/Categories.js
+++ components/Categories.js
@@ -13,7 +13,7 @@ const Categories = () => {
   }, []);
 
   return (
-    <div className="bg-white shadow-lg rounded-lg p-8 pb-12 mb-4">
+    <div className="bg-white shadow-lg rounded-lg p-8 pb-12 mb-3.5">
       <h3 className="text-xl mb-8 font-semibold border-b pb-4">Categories</h3>
       {categories.map((category, index) => (
         <Link key={index} href={`/category/${category.slug}`}>
