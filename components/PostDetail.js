521e3e7d689cc6f24e0def8b2f77cc1ae463997f
Michał Potoczny <michal.potoczny.92@gmail.com>
2021-12-17 08:24:45 +0000

    styling

--- components/PostDetail.js
+++ components/PostDetail.js
@@ -54,17 +54,17 @@ const PostDetail = ({ post }) => {
       <div className="px-4 lg:px-0">
         <div className="flex justify-between items-center mb-12 w-full">
           <div className="flex items-center lg:mb-0 mr-8">
-            <img 
+            {/*<img 
               src={post.author.photo.url} 
               alt={post.author.name} 
               width="30px"
               height="30px"
               className="align-middle rounded-full"
-            />
+            />*/}
             <p className="inline align-middle text-gray-700 ml-2 text-lg">{post.author.name}</p>
           </div>
           <div className="font-medium items-center text-gray-700">
-            <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6 inline mr-2 text-pink-500" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+            <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6 inline mr-2 mb-1 text-pink-500" fill="none" viewBox="0 0 24 24" stroke="currentColor">
               <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M8 7V3m8 4V3m-9 8h10M5 21h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v12a2 2 0 002 2z" />
             </svg>
             <span>
