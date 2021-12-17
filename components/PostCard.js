90963f42ba3e9a90144d9b25e97189597a882421
Michał Potoczny <michal.potoczny.92@gmail.com>
2021-12-17 09:52:19 +0000

    styling

--- components/PostCard.js
+++ components/PostCard.js
@@ -5,7 +5,7 @@ import Link from 'next/link'
 const PostCard = ({ post }) => {
   console.log(post);
   return (
-    <div className="bg-white shadow-lg rounded-lg p-0 lg:p-8 pb-12 mb-8">
+    <div className="bg-white shadow-lg rounded-lg p-0 lg:p-8 pb-12 mb-8 cursor-pointer">
       <Link href={`/post/${post.slug}`}>
         <div className="relative overflow-hidden shadow-md pb-80 mb-6">
           <img 
