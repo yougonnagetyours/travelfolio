6a18eb344e3d2a2155e93cdbe3a136e08bb0ef7a
Michał Potoczny <michal.potoczny.92@gmail.com>
2021-12-17 08:17:53 +0000

    once more

--- pages/post/[slug].js
+++ pages/post/[slug].js
@@ -1,5 +1,5 @@
 import React from 'react'
-
+import Head from 'next/head'
 import { getPosts, getPostDetails } from '../../services'
 
 import { PostDetail, Categories, PostWidget, Author, Comments, CommentsForm } from '../../components'
