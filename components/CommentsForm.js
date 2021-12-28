51fd8d71e682b16328708a7024d140d1835484e3
yougonnagetyours <michal.potoczny.92@gmail.com>
2021-12-28 13:10:50 +0100

    set commentsForm

--- components/CommentsForm.js
+++ components/CommentsForm.js
@@ -1,4 +1,6 @@
-import React, { useState, useEffect, useRef } from 'react'
+import React, { useState, useEffect, useRef } from 'react';
+
+import { submitComment  } from '../services';
 
 const CommentsForm = ({ slug }) => {
   const [error, setError] = useState(false);
@@ -8,6 +10,11 @@ const CommentsForm = ({ slug }) => {
   const nameEl = useRef();
   const emailEl = useRef();
   const storeDataEl = useRef();
+
+  useEffect(() => {
+    nameEl.current.value = window.localStorage.getItem('name');
+    emailEl.current.value = window.localStorage.getItem('email');
+  }, [])
 
   const handleCommentSubmission = () => {
     setError(false);
@@ -24,11 +31,28 @@ const CommentsForm = ({ slug }) => {
 
     const commentObj = { name, email, comment, slug };
 
+    if(storeData) {
+      window.localStorage.setItem('name', name);
+      window.localStorage.setItem('email', email);
+    } else {
+      window.localStorage.removeItem('name', name);
+      window.localStorage.removeItem('email', email);
+    } 
+    
+    submitComment(commentObj)
+      .then((res) => {
+        setShowSuccessMessage(true);
+        
+        setTimeout(() => {
+          setShowSuccessMessage(false);
+        }, 3000);
+      })
+
   }
 
   return (
     <div className='bg-white shadow-lg rounded-lg p-8 pb-12 mb-8'>
-      <h3 className='text-xl mb-8 font-semibold border-b pb-4'>CommentsForm</h3>
+      <h3 className='text-xl mb-8 font-semibold border-b pb-4'>Leave a reply</h3>
       <div className="grid grid-cols-1 gap-4 mb-4">
         <textarea 
           ref={commentEl} 
