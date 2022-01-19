aad5793063c7352ec51f738d9c6cbd50e63a6096
Michał Potoczny <michal.potoczny.92@gmail.com>
2022-01-19 13:44:21 +0100

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,11 @@ module.exports = {
   purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      animation: {
+        'bounce-once' : 'animation: bounce 1s 1',
+      }
+    },
   },
   variants: {
     extend: {},
